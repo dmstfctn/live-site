a51eee34238c0f6c67d81c3d69087a0ad055b0e0
ruffnuff <64215350+ruffnuff@users.noreply.github.com>
2025-08-29 12:44:52 +0200

    Update Config.js

--- assets/js/src/modules/Config.js
+++ assets/js/src/modules/Config.js
@@ -1,11 +1,11 @@
 const Config = {
   SITE_SHOULD_LOOP: true,
-  BREAKPOINT: 900,
+  BREAKPOINT: 1000,
   TRANSITION_HIDE_CLASS: 'dc-transition--out',
   TRANSITION_ACTIVE_CLASS: 'dc-transition--target',
   URL_ROOT: '||**URL_ROOT**||',
   NAME: '||**NAME**||',
-  LANDER_IMG_TIMING: 6000
+  LANDER_IMG_TIMING: 5000
 };
 
 module.exports = Config;
