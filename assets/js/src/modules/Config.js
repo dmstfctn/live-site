4cbf7c2c2abcaedb72dbccd78c540e8936c497e3
O <o@dmstfctn.net>
2025-08-29 10:45:08 +0100

    Update Config.js - add lander image timing

--- assets/js/src/modules/Config.js
+++ assets/js/src/modules/Config.js
@@ -4,7 +4,8 @@ const Config = {
   TRANSITION_HIDE_CLASS: 'dc-transition--out',
   TRANSITION_ACTIVE_CLASS: 'dc-transition--target',
   URL_ROOT: '||**URL_ROOT**||',
-  NAME: '||**NAME**||'
+  NAME: '||**NAME**||',
+  LANDER_IMG_TIMING: 6000
 };
 
 module.exports = Config;
